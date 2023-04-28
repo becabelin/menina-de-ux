a7bc58bb42caf83c108582e9d55676743b71cea9
Joao Teles <joaojoseteles@gmail.com>
2023-04-28 15:41:55 -0300

    :recycle: refactor: adicionando metaTag viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <body>
         <Main />
         <NextScript />
