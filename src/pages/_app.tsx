f2b78f260144d183107065023e25055c5bddc7c3
Joao Teles <joaojoseteles@gmail.com>
2023-05-01 15:49:59 -0300

    :recycle: refactor: utilizando Componente Layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+// Components
+import Layout from "@/components/layout";
+
+// Styles
+import "@/styles/globals.css";
+
+// Utilities
+// Hooks
+
+// Interfaces
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
