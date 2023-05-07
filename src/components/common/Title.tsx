c901e888adbd55d77588dde399e9bc412854b609
Joao Teles <joaojoseteles@gmail.com>
2023-05-07 17:27:14 -0300

    :sparkles: feat: Component Title

---
+++ src/components/common/Title.tsx
@@ -0,0 +1,17 @@
+// Components
+// Styles
+// Utilities
+// Hooks
+
+// Interfaces
+import { ITitle } from "@/interface/components";
+
+export default function Title({ type, children, style }: ITitle) {
+  // Verifica se o título é do tipo principal. Se for, a tag será h1. Se não, será h2.
+  const Comp = type === "Primary" ? "h1" : "h2";
+  return (
+    <>
+      <Comp className={`${style}`}>{children}</Comp>
+    </>
+  );
+}
