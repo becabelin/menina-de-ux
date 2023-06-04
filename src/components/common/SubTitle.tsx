e2cd023db555e55e59c65b7f57f40ed95a72a518
Joao Teles <joaojoseteles@gmail.com>
2023-06-04 11:54:56 -0300

    :sparkles: feat: Componente SubTitle

---
+++ src/components/common/SubTitle.tsx
@@ -0,0 +1,15 @@
+// Components
+// Styles
+// Utilities
+// Hooks
+
+// Interfaces
+import { ISubTitle } from "@/interface/components";
+
+export default function SubTitle({ children, style }: ISubTitle) {
+  return (
+    <>
+      <p className={`${style}`}>{children}</p>
+    </>
+  );
+}
