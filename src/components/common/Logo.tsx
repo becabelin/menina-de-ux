4fa984c5095fa167c595d2eafbac75514f988c3e
Joao Teles <joaojoseteles@gmail.com>
2023-06-04 12:10:56 -0300

    :mag:ajustando a prioridade da imagem da logo

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -12,7 +12,7 @@ export default function Logo({ src, href, width, height, alt }: ILogo) {
   return (
     <>
       <Link href={href}>
-        <Image src={src} width={width} height={height} alt={alt} />
+        <Image src={src} width={width} height={height} alt={alt} priority />
       </Link>
     </>
   );
