8472c78bb6f9410d83a4b4862c52ff687efafafb
Mariana Leao <mariana.ramosl@hotmail.com>
2023-08-09 13:25:52 -0300

    :recycle: refactor: componente cardLinks

--- src/components/common/Links/card/cardLinks.tsx
+++ src/components/common/Links/card/cardLinks.tsx
@@ -1,12 +1,23 @@
 import { ICardLinks } from '@/interface/components'
 import styles from './index.module.scss'
 
-export default function CardLinks({ emoji, link, children }: ICardLinks) {
+export default function CardLinks({
+  emoji,
+  link,
+  children,
+  principal,
+}: ICardLinks) {
   return (
     <div className={styles.card_container}>
       <a href={link} target="_blank" rel="noopener noreferrer">
-        <div className={styles.card}>
-          <div className={styles.card__text}>{children}</div>
+        <div className={`${principal ? styles.card__principal : styles.card}`}>
+          <div
+            className={`${
+              principal ? styles.card__principal : styles.card__text
+            }`}
+          >
+            {children}
+          </div>
           <div className={styles.card__emoji}>{emoji}</div>
         </div>
       </a>
