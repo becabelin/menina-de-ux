dfe73e31becddfb54bb862d01710f0cf84464d07
Lucivania Ventura <97102584+lucivania-ventura@users.noreply.github.com>
2023-08-26 17:04:14 -0300

    :recycle: refactor: Atualizacao do componente Card

--- src/components/common/Card/index.tsx
+++ src/components/common/Card/index.tsx
@@ -1,6 +1,7 @@
 // Components
 // Styles
 import styled from './index.module.scss'
+
 // Utilities
 import Image from 'next/image'
 import Link from 'next/link'
@@ -12,41 +13,37 @@ import { convertStringCharacters } from '@/utilities/Converts'
 import { ICard } from '@/interface/components'
 
 export default function Card(props: ICard) {
-  const { alt, src, title, description, href, subtitle, type = 'base' } = props
+  const { alt, src, title, description, subtitle, href, type = 'base' } = props
 
   const style = type === 'Primary' ? styled.primary : styled.secondary
 
   return (
-    <>
-      <div className={style}>
-        <Image
-          src={`${src}`}
-          width={type === 'Primary' ? 300 : 240}
-          height={type === 'Primary' ? 150 : 120}
-          alt={alt}
-          className={styled.img}
-        />
-        <div className={styled.divCardContent}>
-          <div className={styled.divCardText}>
-            <p className={styled.title}>
-              {title.length > 300
-                ? `${convertStringCharacters(title, 48)}...`
-                : title}
-            </p>
-            {subtitle && <span className={styled.subtitle}>{subtitle}</span>}
-            {description && (
-              <p className={styled.description}>
-                {description.length > 280
-                  ? `${convertStringCharacters(description, 280)}...`
-                  : description}
-              </p>
-            )}
-          </div>
-          <Link href={`${href}`} target="_blank" className={styled.link}>
-            Saiba mais <HiOutlineArrowLongRight size={20} />
-          </Link>
-        </div>
-      </div>
-    </>
+    <div className={style}>
+      <Image
+        src={`${src}`}
+        width={type === 'Primary' ? 300 : 240}
+        height={type === 'Primary' ? 150 : 120}
+        alt={alt}
+        className={styled.img}
+      />
+      <h2 className={styled.title}>
+        {title.length > 300
+          ? `${convertStringCharacters(title, 300)}...`
+          : title}
+      </h2>
+      {subtitle && <span className={styled.subtitle}>{subtitle}</span>}
+      {description && (
+        <p className={styled.description}>
+          {description.length > 1000
+            ? `${convertStringCharacters(description, 1000)}...`
+            : description}
+        </p>
+      )}
+      {href && (
+        <Link href={`${href}`} target="_blank" className={styled.link}>
+          Saiba mais <HiOutlineArrowLongRight size={20} />
+        </Link>
+      )}
+    </div>
   )
 }
