cb18744ae7c30cde255a08fba5770bfe6fdbbac9
alvesxdani <dani.alvesm22@gmail.com>
2023-09-09 12:46:24 -0300

    :lipstick: feat: Add div Objetivos para a section Sobre

--- src/pages/sobre.tsx
+++ src/pages/sobre.tsx
@@ -6,6 +6,7 @@ import style from '@/styles/Sobre.module.scss'
 
 // Assets
 import MulherLendo from '../assets/mulher-lendo.webp.png'
+import MulherAssistindo from '../assets/menina-assistindo-aula.webp.png'
 
 // Utilities
 // Hooks
@@ -50,6 +51,46 @@ export default function Sobre() {
         </div>
         <div className={style.sectionHero}>
           <p className={style.textWrapper}>Objetivos da Menina de UX</p>
+        </div>
+        <div className={style.sectionCarousel}>
+          <div className={style.divCarousel}>
+            <div className={style.overlapGroup}>
+              <div className={style.div}>
+                <div className={style.div2}>
+                  <img
+                    className={style.meninaAssistindo}
+                    alt="Menina assistindo"
+                    src={MulherAssistindo.src}
+                  />
+                  <div className={style.div3}>
+                    <p className={style.textWrapper}>
+                      Ensinar e trazer conteúdos incríveis
+                    </p>
+                    <p className={style.p}>
+                      A Menina de UX busca poder ajudar meninas que queiram
+                      começar suas carreiras em design e/ou tecnologia
+                    </p>
+                    <div className={style.textWrapper2}>Por quê?</div>
+                    <p className={style.textWrapper3}>
+                      Porque ela acredita que qualquer menina pode ser o que
+                      quiser e que podemos aprender qualquer coisa
+                    </p>
+                    <div className={style.textWrapper4}>Como?</div>
+                    <p className={style.textWrapper5}>
+                      Criando aulas, apostilas e conteúdos sobre UX e carreira
+                      de forma totalmente gratuita e online!
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className={style.ol}>
+                <div className={style.li} />
+                <div className={style.li2} />
+                <div className={style.li3} />
+                <div className={style.li4} />
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </>
