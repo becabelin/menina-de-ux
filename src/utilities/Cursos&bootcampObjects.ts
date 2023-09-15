d43a63bca647f06e392b74d130dc3e9c5ceb9797
Lídia Sarti <lid.sarti@gmail.com>
2023-09-15 15:16:30 -0300

    🚧 inicio montagem object cursos

--- src/utilities/Cursos&bootcampObjects.ts
+++ src/utilities/Cursos&bootcampObjects.ts
@@ -46,7 +46,83 @@ const bootcampObjetc = [
   },
 ]
 
+const cursoObjetc = [
+  {
+    id: 1,
+    image: 'https://meninadeux.com/assets/img/cursos/1mio.webp',
+    title: '1MiO',
+    description: 'A iniciativa Um Milhão de Oportunidades (1MiO) é a maior articulação do Brasil pela inclusão produtiva de adolescentes e jovens no mundo do trabalho decente. No site tem diversos cursos, eventos e oportunidades de trabalho divulgadas!',
+    link: 'https://1mio.com.br/feeds',
+    category: ['todos'],
+  },
+  {
+    id: 2,
+    image: 'https://meninadeux.com/assets/img/cursos/adobeeduca.webp',
+    title: 'Adobe Educa',
+    description: 'A Adobe Educa é a plataforma de educação gratuita para todos os professores do Brasil! Aqui você encontra cursos com especialistas em todos os programas da Adobe Creative Cloud: design, ilustração, animação, produção de vídeo, editoração e muito mais.',
+    link: 'https://por.adobe-educa.com',
+    category: ['todos'],
+  },
+  {
+    id: 3,
+    image: 'https://meninadeux.com/assets/img/cursos/avamec.webp',
+    title: 'AVAMEC',
+    description: 'A AVAMEC é um ambiente virtual colaborativo de aprendizagem que permite a concepção, administração e desenvolvimento de diversos tipos de ações formativas, como cursos a distância, complemento a cursos presenciais, projetos de pesquisa, projetos colaborativos e diversas outras formas de apoio educacional à distância ao processo ensino-aprendizagem!',
+    link: 'https://avamec.mec.gov.br/#/',
+    category: ['todos'],
+  },
+  {
+    id: 4,
+    image: 'https://meninadeux.com/assets/img/cursos/amazonalexa.webp',
+    title: 'Alexa Skills: Design de Voz',
+    description: 'Aprenda as melhores práticas de design para skills da Alexa, você aprenderá a criar experiências de voz naturais e enriquecedoras.',
+    link: 'https://build.amazonalexadev.com/cursoalexa-designdevoz.html',
+    category: ['todos'],
+  },
+  {
+    id: 5,
+    image: 'https://meninadeux.com/assets/img/cursos/awari.webp',
+    title: 'Cursos Introdutórios na Awari',
+    description: 'A Awari oferece cursos gratuitos em Data Science, Programação, UX/UI design e outros. Para acessá-los, basta você clicar rolar até a seção de "Cursos em destaque"!',
+    link: 'https://awari.com.br/#categorias',
+    category: ['todos'],
+  },
+  {
+    id: 6,
+    image: 'https://meninadeux.com/assets/img/cursos/awsskillbuilder.webp',
+    title: 'AWS Skill Builder',
+    description: 'A Amazon criou o AWS Skill Builder, que é o seu centro de aprendizado para o desenvolvimento das habilidades de nuvem mais desejadas! A plataforma tem vários cursos sobre a cloud AWS',
+    link: 'https://explore.skillbuilder.aws/learn',
+    category: ['todos'],
+  },
+  {
+    id: 7,
+    image: 'https://meninadeux.com/assets/img/cursos/bancocentraldobrasil.webp',
+    title: 'Branco Central do Brasil',
+    description: 'O Banco Central do Brasil está oferecendo dois cursos online e gratuitos sobre finanças: Gestão de Finanças Pessoais e Formação de Multiplicadores da Série "Eu e Meu Dinheiro".',
+    link: 'https://www.bcb.gov.br/cidadaniafinanceira/cursos',
+    category: ['todos'],
+  },
+  {
+    id: 8,
+    image: 'https://meninadeux.com/assets/img/cursos/bolsagratis.webp',
+    title: 'Bolsa Grátis',
+    description: 'Aumente suas possibilidades profissionais com cursos exclusivos, de curta duração e totalmente gratuitos!',
+    link: 'https://bolsagratis.com.br/busca-cursos/?f=&type=cursos_gratis&presencial=true&ead=true',
+    category: ['todos'],
+  },
+  {
+    id: 9,
+    image: 'https://meninadeux.com/assets/img/cursos/cieesabervirtual.webp',
+    title: 'CIEE - Saber Virtual',
+    description: 'O CIEE Saber Virtual é uma plataforma de cursos que permite a realização totalmente a distância. Nesse portal, é possível realizar 5 trilhas de conhecimento, que foram feitas com a intenção de preparar estudantes para o mundo do trabalho, sendo que, aqueles que concluírem os cursos, terão direito a um certificado.',
+    link: 'https://portal.ciee.org.br/para-voce/saber-virtual/',
+    category: ['todos'],
+  },
+]
+
 export const properties = {
   filtros,
   bootcampObjetc,
+  cursoObjetc,
 }
