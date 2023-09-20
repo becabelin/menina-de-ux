7abb75c35c22c782ddfe65a16cc3f132a47c9c81
Mariana Leao <mariana.ramosl@hotmail.com>
2023-09-20 12:26:47 -0300

    :sparkles: feat: Adicionando mock para os card da página Inicial

---
+++ src/utilities/inicalCardObject.ts
@@ -0,0 +1,75 @@
+export const inicialCardObject1 = [
+  {
+    id: 1,
+    type: 'Primary',
+    src: './Inicio/curso-em-video.svg',
+    alt: 'alt img',
+    title: 'Curso em vídeo',
+  },
+
+  {
+    id: 2,
+    type: 'Primary',
+    src: './Inicio/dio.webp.svg',
+    alt: 'alt img',
+    title: 'Digital Innovation One',
+  },
+
+  {
+    id: 3,
+    type: 'Primary',
+    src: './Inicio/estacaohack.webp.svg',
+    alt: 'alt img',
+    title: 'Estação Hack',
+  },
+
+  {
+    id: 4,
+    type: 'Primary',
+    src: './Inicio/mundo-senai.svg',
+    alt: 'alt img',
+    title: 'Mundo SENAI',
+  },
+]
+
+export const inicialCardObject2 = [
+  {
+    id: 1,
+    type: 'Primary',
+    src: './Inicio/soulbilingue.webp.svg',
+    alt: 'alt img',
+    title: 'Soul Bilíngue',
+    subtitle: 'Inscrições até 10/12/22',
+    href: '#',
+  },
+
+  {
+    id: 2,
+    type: 'Primary',
+    src: './Inicio/martechacademy.webp.svg',
+    alt: 'alt img',
+    title: 'Martech Academy',
+    subtitle: 'Inscrições até 28/11/22',
+    href: '#',
+  },
+
+  {
+    id: 3,
+    type: 'Primary',
+    src: './Inicio/Cursos.svg',
+    alt: 'alt img',
+    title: 'Capacitação Gratuita em Introdução à Programação',
+    subtitle: 'Inscrições por tempo limitado!',
+    href: 'https://pages.prozeducacao.com.br/proz-tecnologia',
+  },
+
+  {
+    id: 4,
+    type: 'Primary',
+    src: './Inicio/curso.svg',
+    alt: 'alt img',
+    title: 'Bolsas Santander Tecnologia | Santander Gamer Pro',
+    subtitle: 'Inscrições até 24/11/22',
+    href: '#',
+  },
+]
