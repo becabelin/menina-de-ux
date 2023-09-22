ccca19e46f187a1981c8e23eb93ceb996dc9413a
Mariana Leao <mariana.ramosl@hotmail.com>
2023-09-22 09:23:36 -0300

    :sparkles: feat: Adicionando novo recurso no componente card <a>

--- src/components/common/Card/index.tsx
+++ src/components/common/Card/index.tsx
@@ -1,3 +1,46 @@
+// // Components
+// // Styles
+// import styled from './index.module.scss'
+
+// // Utilities
+// import Link from 'next/link'
+// import { HiOutlineArrowLongRight } from 'react-icons/hi2'
+// import { convertStringCharacters } from '@/utilities/Converts'
+
+// // Hooks
+// // Interfaces
+// import { ICard } from '@/interface/components'
+
+// export default function Card(props: ICard) {
+//   const { alt, src, title, description, subtitle, href, type = 'base' } = props
+
+//   const style = type === 'Primary' ? styled.primary : styled.secondary
+
+//   return (
+//     <div className={style}>
+//       <img src={`${src}`} alt={alt} className={styled.img} />
+//       <h2 className={styled.title}>
+//         {title.length > 300
+//           ? `${convertStringCharacters(title, 300)}...`
+//           : title}
+//       </h2>
+//       {subtitle && <span className={styled.subtitle}>{subtitle}</span>}
+//       {description && (
+//         <p className={styled.description}>
+//           {description.length > 1000
+//             ? `${convertStringCharacters(description, 1000)}...`
+//             : description}
+//         </p>
+//       )}
+//       {href && (
+//         <Link href={`${href}`} target="_blank" className={styled.link}>
+//           Saiba mais <HiOutlineArrowLongRight size={20} />
+//         </Link>
+//       )}
+//     </div>
+//   )
+// }
+
 // Components
 // Styles
 import styled from './index.module.scss'
@@ -12,11 +55,20 @@ import { convertStringCharacters } from '@/utilities/Converts'
 import { ICard } from '@/interface/components'
 
 export default function Card(props: ICard) {
-  const { alt, src, title, description, subtitle, href, type = 'base' } = props
+  const {
+    alt,
+    src,
+    title,
+    description,
+    subtitle,
+    href,
+    type = 'base',
+    ancora = false,
+  } = props
 
   const style = type === 'Primary' ? styled.primary : styled.secondary
 
-  return (
+  const cardContent = (
     <div className={style}>
       <img src={`${src}`} alt={alt} className={styled.img} />
       <h2 className={styled.title}>
@@ -32,11 +84,22 @@ export default function Card(props: ICard) {
             : description}
         </p>
       )}
-      {href && (
-        <Link href={`${href}`} target="_blank" className={styled.link}>
+      {href && !ancora && (
+        <Link href="#" target="_blank" className={styled.link}>
           Saiba mais <HiOutlineArrowLongRight size={20} />
         </Link>
       )}
     </div>
   )
+
+  if (ancora && href) {
+    return (
+      <a href={`${href}`} className={style}>
+        {cardContent}
+      </a>
+    )
+  } else {
+    // Adicione um retorno padrão caso nenhuma condição seja atendida
+    return cardContent
+  }
 }
