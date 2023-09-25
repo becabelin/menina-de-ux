9ad507b6e43a1ee45c66fbc14fb9a6d84cf70a87
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-09-25 11:19:43 -0300

    feat: modified Title Component

--- src/components/common/Title/Title.tsx
+++ src/components/common/Title/Title.tsx
@@ -9,10 +9,11 @@ import styles from './index.module.scss'
 import { ITitle } from '@/interface/components'
 
 export default function Title({ children, type = 'Default' }: ITitle) {
-  // Verifica se o título é do tipo principal. Se for, a tag será h1. Se não, será h2.
+  const TitleType = type === 'Default' ? 'h1' : 'h2'
+
   return (
     <>
-      <h2 className={styles[`title${type}`]}>{children}</h2>
+      <TitleType className={styles[`title${type}`]}>{children}</TitleType>
     </>
   )
 }
