246644675f65d66ad210a9e41fe7e2f1f7bb3831
JacqueKajiya <jacquelinekajiya@gmail.com>
2023-09-25 12:28:46 -0300

    fix: fixed title's size proportion

--- src/components/common/Title/Title.tsx
+++ src/components/common/Title/Title.tsx
@@ -9,6 +9,8 @@ import styles from './index.module.scss'
 import { ITitle } from '@/interface/components'
 
 export default function Title({ children, type = 'Default' }: ITitle) {
+  // Verifica se o título é do tipo principal. Se for, a tag será h1. Se não, será h2.
+
   const TitleType = type === 'Default' ? 'h1' : 'h2'
 
   return (
